0b4ab1972d174d27bf19199fe1f1584d21e367a7
Yoom Davidson <ddavidson202020@gmail.com>
2020-04-07 15:50:56 -0400

    small changes

--- server.js
+++ server.js
@@ -7,7 +7,32 @@ var PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
+
+// API Get route
+app.get("/api/notes", (req, res) => {
+    fs.readFile("db/db.json",  "utf8", (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data))
+    });
+});
+// API Post route
+app.post("/api/notes", (req, res) => {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        const jsonFile = JSON.parse(data)
+        const newNotes = req.body;
+
+        jsonFile.push(newNotes);
+        console.log(jsonFile)
+        fs.writeFile(jsonFile)
+    });
+
+    
+    
+
+    
+});
 // Sending the notes.html page back for the get request (HTML route)
 app.get("/notes", (req, res) => {
     return res.sendFile(path.join(__dirname, "public/notes.html"));
@@ -15,24 +40,6 @@ app.get("/notes", (req, res) => {
 // Sending the index.html back (HTML route)
 app.get("*", (req, res) => {
     return res.sendFile(path.join(__dirname, "public/index.html"));
-});
-// API Get route
-app.get("/api/notes", (req, res) => {
-    let jsonFile = fs.readFile("db/db.json",  "utf8", () => {
-        if (err) throw err;
-    });
-    return res.json(jsonFile);
-});
-// API Post route
-app.post("/api/notes", (req, res) => {
-    let jsonFile = fs.readFile("db/db.json", "utf8", () => {
-        if (err) throw err;
-    });
-    console.log(jsonFile);
-    const newNotes = req.body;
-    jsonFile.push(newNotes);
-
-    
 });
 // Starts the server to begin listening
 app.listen(PORT, () => {
